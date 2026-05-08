2a49dea5cca4e8bf2e9853678793e395659cdf0a
Javeed Mohammed <javeedmohd214@gmail.com>
2026-05-08 18:35:42 +0530

    jenkins

---
+++ vars/testPipeline.groovy
@@ -0,0 +1,32 @@
+def call(Map configMap) {
+    pipeline {
+        agent {
+            node {
+                label 'roboshop'
+            }
+        }
+        stages {
+            stage('testing') {
+                steps {
+                    script{
+                        sh """
+                            echo "project: ${configMap.project}"
+                        """
+                    }
+                }
+            }
+        }
+        // post build
+        post { 
+            always { 
+                echo 'I will always say Hello again!'
+            }
+            success { 
+                echo 'Pipeline Success'
+            }
+            failure { 
+                echo 'Pipeline Failure'
+            }
+        }
+    }
+} 
